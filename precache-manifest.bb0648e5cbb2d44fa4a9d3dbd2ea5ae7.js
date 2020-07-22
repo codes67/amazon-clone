5df5e4d8c2da656f6187bb404ba70ad9fbe12178
Yogesh Pradhan <yogeshpradhan@Yogeshs-Air.lan1>
2020-07-22 15:45:56 -0400

    Updates

---
+++ precache-manifest.bb0648e5cbb2d44fa4a9d3dbd2ea5ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0268db3278242828653ead9a84377c",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "fc2dd5323558f2928ee0",
+    "url": "/amazon-clone/static/css/main.4ebaf152.chunk.css"
+  },
+  {
+    "revision": "528fceb43ad0d9bed893",
+    "url": "/amazon-clone/static/js/2.c5fe2b3e.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/amazon-clone/static/js/2.c5fe2b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2dd5323558f2928ee0",
+    "url": "/amazon-clone/static/js/main.c70e702f.chunk.js"
+  },
+  {
+    "revision": "171385e1e67485d2c77f",
+    "url": "/amazon-clone/static/js/runtime-main.30fe8631.js"
+  }
+]);
